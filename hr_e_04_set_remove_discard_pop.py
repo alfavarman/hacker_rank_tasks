a4ed1593148cc41e12694bcc9f807dadc9512808
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-20 12:14:31 +0700

    change dictionary keys to be str not int

---
+++ hr_e_04_set_remove_discard_pop.py
@@ -0,0 +1,6 @@
+num_of_elements = int(input())
+set_elements_separate_space = set(map(int, input().split()))
+num_commands = int(input())
+for i in range(num_commands):
+    x = input()
+    if x 'remove'
