6a77d91f708275e732b8e82f7f72fd07a1cf99dd
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-06-22 16:01:08 +0700

    second highest result in the list

--- hr_07_find_the_runner_up_score.py
+++ hr_07_find_the_runner_up_score.py
@@ -24,3 +24,10 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
+    arr = list(set(arr))
+    arr.sort()
+    arr.pop()
+    print(arr.pop())
+
+
+# other methods
